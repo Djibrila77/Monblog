2d5bb6e268dcffd563f1a2f812204b736325859c
Djibrila77 <djibrila6299@gmail.com>
2026-02-08 17:43:40 +0000

    Correction :  sende mail e

--- config/config/settings.py
+++ config/config/settings.py
@@ -123,7 +123,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_TIMEOUT = 10
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
